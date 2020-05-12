36273cae8990b0baa41cc913362489033b140e30
zhang <https://github.com/userxizi/springcloud.git>
2020-05-12 16:09:04 +0800

    spring-cloud 使用Ribbon 实现客户端的负载均衡，并且书写轮询策略的负载均衡算法。 负载均衡算法 请求次数 % 服务器集群总数 =实际调用服务器的下标（即调用哪个服务器） 每次服务器重启之后，请求次数会重置。

--- cloud-consumer-order80/src/main/java/com/zrx/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zrx/springcloud/controller/OrderController.java
@@ -2,20 +2,32 @@ package com.zrx.springcloud.controller;
 
 import com.zrx.springcloud.entities.CommonResult;
 import com.zrx.springcloud.entities.Payment;
+import com.zrx.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class OrderController {
 
 //    public static final String PAYMENT_URL="http://localhost:8001";
-      public static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
+    public static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
+    private LoadBalancer loadBalancer;
 
     @Resource
     private RestTemplate restTemplate;
@@ -31,4 +43,44 @@ public class OrderController {
         log.info("=========客户端调用服务端 查询=========="+id);
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") long id){
+        log.info("=========客户端调用服务端 查询=========="+id);
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+           return new CommonResult<>(444,"查询操作失败");
+        }
+    }
+
+//    @GetMapping("/consumer/payment/postForEntity")
+//    public CommonResult<Payment> create1(Payment payment){
+//        log.info("=========客户端调用服务端 添加=========="+payment);
+//        CommonResult body = restTemplate.postForEntity(PAYMENT_URL + "/payment/get/", payment, CommonResult.class).getBody();
+//        log.info("=========客户端调用服务端 添加=========="+body);
+//        return body;
+//    }
+//
+//    @GetMapping("/consumer/payment/postForObject")
+//    public CommonResult<Payment> create2(Payment payment){
+//        log.info("=========客户端调用服务端 添加=========="+payment);
+//        CommonResult body = restTemplate.postForObject(PAYMENT_URL + "/payment/get/", payment, CommonResult.class);
+//        log.info("=========客户端调用服务端 添加=========="+body);
+//        return body;
+//    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0){
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/lb",String.class);
+    }
+
+
 }
