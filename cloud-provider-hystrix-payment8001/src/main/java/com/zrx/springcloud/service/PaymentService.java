1b223b65efd11d729ce0d88648efe2654a8d5760
zhang <https://github.com/userxizi/springcloud.git>
2020-05-14 15:49:09 +0800

    spring-cloud 使用Hystrix 实现 服务熔断 （服务熔断分为三个状态：） 熔断打开：请求不再进行调用当前服务，内部设置时钟一般为MTTR(平均故障处理时间)，当打开时长达到所设时钟则进入熔断状态 熔断关闭：熔断关闭不会对服务进行熔断 熔断半开：部分请求根据规则调用当前服务，如果请求成功且符合规则则认为当前服务恢复正常，关闭熔断 服务熔断类似于保险丝装置，在一定时间内请求次数的错误达到了配置的要求，则进行服务熔断操作，进入熔断半开，在过一段时间，请求如果回复正常则逐渐恢复调用。

--- cloud-provider-hystrix-payment8001/src/main/java/com/zrx/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zrx/springcloud/service/PaymentService.java
@@ -8,4 +8,6 @@ public interface PaymentService {
     public String paymentInfo_OK(Integer id);
 
     public String paymentInfo_TimeOut(Integer id);
+
+    public String paymentCircuitBreaker(Integer id);
 }
