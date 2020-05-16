4a1a3d135b9f96df123931c2f735f1b536233f7f
zhang <https://github.com/userxizi/springcloud.git>
2020-05-16 15:35:51 +0800

    spring-cloud 使用Gateway 为8001 8002 进行转发，使用动态路由进行转发 配置使用Filter 拦截器，使用Gateway自带的拦截器配置，和配置我们自己的拦截器配置。配置我们自己的拦截器配置需要实现GlobalFilter, Ordered个接口。

---
+++ cloud-gateway-gateway9527/src/main/java/com/zrx/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,38 @@
+package com.zrx.springcloud.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("*********come in MyLogGateWayFilter: "+new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if(StringUtils.isEmpty(username)){
+            log.info("*****用户名为不正确！！！！ 非法用户,(┬＿┬)");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);//给人家一个回应
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+//        加载过滤器顺序，数值越小，过滤器加载的级别越高
+        return 0;
+    }
+}
+
