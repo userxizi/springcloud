1486e3911f8bb04ae97ec7b9f7fcfe2ae114b84c
zrx <zrx_git@zhang.com>
2020-05-20 15:26:32 +0800

    spring-cloud 使用config 作为配置中心 连接github 托管代码，配置自动刷新 避免服务器重启的情况下读取github上的代码，首先POM引入actuator监控，修改YML，暴露监控端口，@RefreshScope业务类Controller修改 完成基础的 自动刷新配置，这样可以避免服务器重启。

--- cloud-config-center-3366/src/main/java/com/zrx/springcloud/controller/ConfigClientController.java
+++ cloud-config-center-3366/src/main/java/com/zrx/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.zrx.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //提供监控刷新功能
 public class ConfigClientController {
 
     @Value("${config.info}")
