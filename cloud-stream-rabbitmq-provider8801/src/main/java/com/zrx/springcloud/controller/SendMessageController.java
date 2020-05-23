6e9946d80eac13766040a35583eb62a3a75ad3f4
zrx <zrx_git@zhang.com>
2020-05-23 15:52:03 +0800

    spring-cloud SpringCloud Stream消息驱动 使用此框架类似于适配器对各种消息队列框架进行适配，避免使用时出现差别，创建服务端用于发送消息，创建客户端用于接收消息，为了避免消息被重复消费，我们使用分组方式对消息的接收者也就是客户端进行分组，实现一个消息只能消费一次，使用在application.yml文件中 配置group: 分组名称 进行消息分组

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/zrx/springcloud/controller/SendMessageController.java
@@ -0,0 +1,19 @@
+package com.zrx.springcloud.controller;
+
+
+import com.zrx.springcloud.secvice.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/zrx/springcloud/secvice/IMessageProvider.java
@@ -0,0 +1,5 @@
+package com.zrx.springcloud.secvice;
+
+public interface IMessageProvider {
+    public String send();
+}
