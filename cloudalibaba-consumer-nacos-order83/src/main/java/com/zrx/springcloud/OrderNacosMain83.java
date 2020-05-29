53f196c4be2557c20dede7861f85d4c9291bf1eb
zrx <zrx_git@zhang.com>
2020-05-29 15:38:20 +0800

    spring-clou  使用Nacos作为服务注册中心，上一次注册完成9001.9002微服务提供者，此次完成83微服务消费者的构建，因为Nacos集成了Ribbon 作为负载均衡，所以可以直接使用负载均衡机制，使用83消费者调用微服务提供者。

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/zrx/springcloud/OrderNacosMain83.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/zrx/springcloud/OrderNacosMain83.java
@@ -1,13 +1,16 @@
 package com.zrx.springcloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class PaymentMain90012 {
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentMain90012.class,args);
+public class OrderNacosMain83
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderNacosMain83.class,args);
     }
 }
