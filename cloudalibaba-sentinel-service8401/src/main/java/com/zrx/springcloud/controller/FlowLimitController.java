c53a98f7b942c37bf9b615b08a2e0253d3521adb
zrx <zrx_git@zhang.com>
2020-06-04 13:27:02 +0800

    spring-clou  Sentinel 做为监控，对服务发访问进行流量控制，保护服务不被过大的访问量冲击导致不可用。 三种流量控制模式 直接 关联 链路 三种流量控制效果 直接失败  Warm Up  排队等待

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zrx/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,26 @@
+package com.zrx.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName()+"\t"+"testB=-=-=-=-=-=-=-=");
+        return "------testB";
+    }
+
+}
