430c6236851dc1c95545592560950fb01434dc1a
zrx <zrx_git@zhang.com>
2020-06-08 17:11:24 +0800

    spring-clou  Sentinel 做降级配置 通过 RT 异常比例 异常数 的大于或者小于阀值来使服务进行降级保护。

--- cloudalibaba-sentinel-service8401/src/main/java/com/zrx/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zrx/springcloud/controller/FlowLimitController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @Slf4j
 public class FlowLimitController {
@@ -23,4 +25,31 @@ public class FlowLimitController {
         return "------testB";
     }
 
+    @GetMapping("/testC")
+    public String testC()
+    {
+
+        log.info("testD 测试异常比例");
+        int age = 10/0;
+        return "------testC";
+    }
+
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");
+
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+
+    }
 }
