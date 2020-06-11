906ae1a406956f056769f49f5d743fde994bc4f6
zrx <zrx_git@zhang.com>
2020-06-11 12:11:05 +0800

    spring-clou sentinel整合ribbon+openFeign+fallback 实现服务熔断功能 sentinel整合Feign系列 实现服务熔断功能

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/zrx/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.zrx.springcloud.service;
+
+import com.zrx.springcloud.entities.CommonResult;
+import com.zrx.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id)
+    {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
+
